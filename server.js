f40601c14e2fe7002e4035c157da517d58a7c2e0
Connor Semb <polishmustard710@gmail.com>
2022-05-19 17:25:50 -0500

    Changed index.html to link properly to notes.html

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, '/public/index.html'));
+});
+
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(_dirname, '/db/db.json'));
 });
@@ -21,10 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '/public/index.html'));
-});
 
-// app.listen(PORT, () =>
-//     console.log(`App listening at http://localhost:${PORT}!`)
-// );
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}!`)
+);
