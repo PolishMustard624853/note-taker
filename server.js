4d014ee99665767845062b26da57fcc7edcf825c
Connor Semb <polishmustard710@gmail.com>
2022-06-08 14:55:13 -0500

    updated dependencies

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
