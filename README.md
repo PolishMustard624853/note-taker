e85f43546dd8347f20217e08a0c7da8b1432b5f5
Connor Semb <polishmustard710@gmail.com>
2022-06-08 14:50:12 -0500

    fixed server.js

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # 11 Express.js: Note Taker
 
 This assignment was to create a note-taker application using express.
-I was able to install express and get the server to run. What I can not figure out is why my notes.html looks so odd even though those were files I had not touched because they were given to us. You can open html in live server but the notes do not save. I am unsure as to why so I am hoping for some feedback on this because I feel I am very close to having this be a functional webpage.
+I could not figure out what to do next, so I do not have a functional webpage resulting in me not having a screenshot or video.... I will return to this assignment at a later time
 ____________________________
 git repository
 https://github.com/PolishMustard624853/note-taker
 
 heroku repository
 https://dashboard.heroku.com/apps/cjs-note-taker
-
-Video Walkthrough
-https://app.vidcast.io/share/c2ea255d-cbfe-43fc-b228-542361184983
 
 
 end_____________________________________________________________________________
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 3000;
+const noteList = require('./db/db.json');
 
 const app = express();
 
@@ -9,16 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '/public/index.html'));
-});
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, '/db/db.json'));
+    res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 
@@ -30,14 +28,13 @@ app.post('/api/notes', (req, res) => {
     return console.log('You saved a new note!')
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
-    noteList = noteList.filter(selected =>{
-        return selected.id != noteId;
-    })
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+app.get('/api/notes', (req, res) => {
     res.json(noteList);
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
